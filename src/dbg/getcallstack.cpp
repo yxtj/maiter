df41d7312e480d3b055a83348bd2bf2bb0bfee8d
yxtj <yanxiangtianji@gmail.com>
2015-12-17 22:52:38 -0500

    fix a crash bug of getcallstack()

--- src/dbg/getcallstack.cpp
+++ src/dbg/getcallstack.cpp
@@ -14,14 +14,15 @@
 
 using namespace std;
 
+static constexpr int max_frames=128;
+static constexpr size_t max_funcnamesize = 2048;
+
 std::string getcallstack(){
 	ostringstream out;
 	out<<"stack trace:";
 
-	static constexpr int max_frames=63;
 	// storage array for stack trace address data
 	void* addrlist[max_frames + 1];
-
 	// retrieve current stack addresses
 	int addrlen = backtrace(addrlist, sizeof(addrlist) / sizeof(void*));
 
@@ -32,21 +33,15 @@ std::string getcallstack(){
 	// resolve addresses into strings containing "filename(function+address)",
 	// this array must be free()-ed
 	char** symbollist = backtrace_symbols(addrlist, addrlen);
-
 	// allocate string which will be filled with the demangled function name
-	size_t funcnamesize = 256;
-//	char* funcname = (char*)malloc(funcnamesize);
-	char funcname[256];
-
-//	for(int i=1;i<addrlen;++i){
-//		out<<symbollist[i]<<"\n";
-//	}
-//	return out.str();
+//	char funcname[max_funcnamesize];
+	char* funcname=(char*)malloc(max_funcnamesize);
+	size_t funcnamesize=max_funcnamesize;
 
 	// iterate over the returned symbol lines. skip the first, it is the
 	// address of this function.
 	for(int i = 1; i < addrlen; i++){
-//		out<<"\n"<<symbollist[i];
+//		out<<"\n"<<symbollist[i]; continue;
 		char *begin_name = 0, *begin_offset = 0, *end_offset = 0;
 
 		// find parentheses and +address offset surrounding the mangled name:
@@ -75,24 +70,20 @@ std::string getcallstack(){
 			int status;
 			char* ret = abi::__cxa_demangle(begin_name,funcname, &funcnamesize, &status);
 			if(status == 0){
-//				funcname = ret; // use possibly realloc()-ed string
-//				fprintf(out, "  %s : %s+%s\n",symbollist[i], funcname, begin_offset);
-//				out<<"  "<<symbollist[i]<<" : "<<funcname<<"+"<<begin_offset<<"\n";
-				out<<"\n  "<<symbollist[i]<<" : "<<ret<<"+"<<begin_offset;
+				funcname = ret; // use possibly realloc()-ed string
+				out<<"\n  "<<symbollist[i]<<" : "<<funcname<<"+"<<begin_offset;
 			}else{
 				// demangling failed. Output function name as a C function with
 				// no arguments.
-//				fprintf(out, "  %s : %s()+%s\n",symbollist[i], begin_name, begin_offset);
 				out<<"\n  "<<symbollist[i]<<" : "<<begin_name<<"()+"<<begin_offset;
 			}
 		}else{
 			// couldn't parse the line? print the whole line.
-//			fprintf(out, "  %s\n", symbollist[i]);
 			out<<"\n  "<<symbollist[i];
 		}
 	}
 
-//	free(funcname);
-//	free(symbollist);
+	free(funcname);
+	free(symbollist);
 	return out.str();
 }
