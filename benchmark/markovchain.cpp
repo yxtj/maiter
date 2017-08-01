a97d0aa71d789749795782c25958c97bc911370c
yxtj <yanxiangtianji@gmail.com>
2017-08-01 08:20:13 +0100

    add markov chain benchmark & fix bug for other benchmark

--- benchmark/markovchain.cpp
+++ benchmark/markovchain.cpp
@@ -9,27 +9,28 @@
 
 using namespace std;
 
-vector<float> cal_pr(const vector<vector<int> >& g, const float damp, const int maxIter, const double epsilon) {
+vector<float> cal_mc(const vector<vector<Edge>>& g, const int maxIter, const double epsilon) {
 	size_t n=g.size();
 
-	vector<float> res(n, 1-damp);
+	vector<float> res(n, 1.0/n);
 	vector<float> old;
 	
 	int iter = 0;
-	double sum=n*static_cast<double>(1-damp);
+	// because the summation is always 1, we use sum of square here
+	double sum=1.0/n; // = n* (1/n)^2
 	double oldsum=0;
 	while(++iter < maxIter && abs(sum-oldsum) > epsilon){
 		old.swap(res); // old=move(res);
 		oldsum=sum;
-		res.assign(n, 1-damp);
-		sum=n*static_cast<double>(1-damp);
+		res.assign(n, 0);
+		sum=0;
 		for(size_t i=0;i<n;++i){
-			const vector<int>& line=g[i];
-			float out=damp*old[i]/line.size();
-			for(int dst : line){
-				res[dst]+=out;
+			const float v = old[i];
+			for(const Edge& e: g[i]){
+				float t=v*e.weight;
+				res[e.node]+=t;
+				sum+=t*t;
 			}
-			sum+=damp*old[i];
 		}
 	}
 	cout<<"  iterations: "<<iter<<"\tdifference: "<<sum-oldsum<<endl;
@@ -38,34 +39,30 @@ vector<float> cal_pr(const vector<vector<int> >& g, const float damp, const int 
 
 int main(int argc, char* argv[]){
 	if(argc<=3){
-		cerr<<"Calculate PageRank."<<endl;
-		cerr<<"Usage: <#parts> <in-folder> <out-folder> [dump-factor] [normalize] [max-iter] [epsilon]\n"
+		cerr<<"Calculate the stationary distribution of a Markov Chain."<<endl;
+		cerr<<"Usage: <#parts> <in-folder> <out-folder> [normalize] [max-iter] [epsilon]\n"
 			<<"  <in-folder>: input file prefix, file name: 'part<id>' is automatically used\n"
 			<<"  <out-folder>: output file prefix, file name 'part-<id>' is automatically used\n"
-			<<"  [damp-factor]: (=0.8) the damping factor (the portion of values transitted) for PageRank\n"
-			<<"  [normalize]: (=0) normalized the result before output\n"
+			<<"  [normalize]: (=0) whether to output the normalized result, otherwise output <n-node> * <real-result> \n"
 			<<"  [max-iter]: (=inf) the maximum number of iterations until termination\n"
-			<<"  [epsilon]: (=1e-6) the minimum difference between consecutive iterations for termination check"
+			<<"  [epsilon]: (=1e-9) the minimum difference between consecutive iterations for termination check"
 			<<endl;
 		return 1;
 	}
 	int parts=stoi(argv[1]);
 	string inprefix=argv[2];
 	string outprefix=argv[3];
-	float damp=0.8;
-	if(argc>4)
-		damp=stof(argv[4]);
-	int maxIter=numeric_limits<int>::max();
-	bool normalize=false;
-	if(argc>5){
-		string opt(argv[5]); // cannot use argv[4]=="x" (this is comparing the pointer)
+	bool normalize=true;
+	if(argc>4){
+		string opt(argv[4]); // cannot use argv[4]=="x" (this is comparing the pointer)
 		normalize=(opt=="1" || opt=="y" || opt=="t" || opt=="yes" || opt=="true");
 	}
+	int maxIter=numeric_limits<int>::max();
+	if(argc>5)
+		maxIter=stoi(argv[5]);
+	double termDiff=1e-9;
 	if(argc>6)
-		maxIter=stoi(argv[6]);
-	double termDiff=1e-6;
-	if(argc>7)
-		termDiff=stod(argv[7]);
+		termDiff=stod(argv[6]);
 	
 	chrono::time_point<std::chrono::system_clock> start_t;
 	chrono::duration<double> elapsed;
@@ -73,11 +70,11 @@ int main(int argc, char* argv[]){
 	// load
 	cout<<"loading graph"<<endl;
     start_t = chrono::system_clock::now();
-	vector<vector<int>> g;
+	vector<vector<Edge>> g;
 	for(int i=0;i<parts;++i){
 		string fn=inprefix+"/part"+to_string(i);
 		cout<<"  loading "<<fn<<endl;
-		if(!load_graph_unweight(g, fn)){
+		if(!load_graph_weight(g, fn)){
 			cerr<<"Error: cannot open input file: "<<fn<<endl;
 			return 3;
 		}
@@ -85,14 +82,24 @@ int main(int argc, char* argv[]){
     elapsed = chrono::system_clock::now()-start_t;
 	cout<<"  load "<<g.size()<<" nodes in "<<elapsed.count()<<" seconds"<<endl;
 
+	// normalize transition distribution
+	cout<<"normalizing the input transition distribution"<<endl;
+	for(auto& line: g){
+		float sum=0.0f;
+		for(Edge& e : line)
+			sum+=e.weight;
+		for(Edge& e : line)
+			e.weight/=sum;
+	}
+
 	// calculate
 	cout<<"calculating"<<endl;
 	start_t = chrono::system_clock::now();
-	vector<float> res = cal_pr(g, damp, maxIter, termDiff);
-	if(normalize){
-		int n=res.size();
-		for(float& v : res)
-			v/=n;
+	vector<float> res = cal_mc(g, maxIter, termDiff);
+	if(!normalize){
+		int n=g.size();
+		for(auto& v : res)
+			v*=n;
 	}
     elapsed = chrono::system_clock::now()-start_t;
 	cout<<"  finished in "<<elapsed.count()<<" seconds"<<endl;
