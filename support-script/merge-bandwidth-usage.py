cbd61f25c2ffd3e893d7e1563a92729ecbf5ab35
yxtj <yanxiangtianji@gmail.com>
2017-10-21 10:25:23 +0800

    add unit converting function to bandwidth script

--- support-script/merge-bandwidth-usage.py
+++ support-script/merge-bandwidth-usage.py
@@ -41,36 +41,49 @@ def gather(folder, do_average, trans_bit):
             res[i]*=8
     return res
     
-def dump_result(output, res):
-    if len(output)==0:
-        for v in res:
-            print(str(v)+' ', end='')
-        print('')
-    else:
+def dump_result(output, trans_unit, res):
+    f=sys.stdout
+    if len(output)!=0:
         f=open(output,'w')
+    if trans_unit==1:
         for v in res:
-            f.write(str(v)+' ')
+            print(str(v)+'\t', end='', file=f)
+    else:
+        for v in res:
+            print('%.2f\t' % (v/trans_unit), end='', file=f)
+    print('', file=f)
+    if len(output)!=0:
         f.close()
 
-def main(folder, output, do_average, trans_bit):
+def main(folder, output, do_average, trans_unit, trans_bit):
     res = gather(folder, do_average, trans_bit)
-    dump_result(output, res)
+    dump_result(output, trans_unit, res)
 
 if __name__=='__main__':
-    if len(sys.argv) <= 1:
+    argc=len(sys.argv)
+    if argc <= 1:
         print('Merge the bandwidth usage records.')
-        print('Usage: <bandwidth-folder> [result-name] [do-average] [trans-to-bit]')
+        print('Usage: <bandwidth-folder> [result-name] [do-average] [trans-to-kmg] [trans-to-bit]')
         print('  <bandwidth-folder>: The folder storing the bandwidth usage records. Their names should start with "band-"')
         print('  [result-name]: (="") The output file. If not give, the output will be put to console. (you may want to use > to redirect)')
         print('  [do-average]: (=0) Output the averaged bandwidth usage for all workers instead of their sum')
+        print('  [trans-to-kmg]: (="") Translate teh value to Kilo (k), Million (m) or Giga (g). Accept: "", 1, k, m, g')
         print('  [trans-to-bit]: (=0) Translate the Byte data to Bit (*8 to each value)')
         exit()
-    argc=len(sys.argv)
     folder=sys.argv[1]
     output=sys.argv[2] if argc > 2 else ""
     do_average=sys.argv[3] in ['1', 'y', 'yes', 't', 'true'] if argc > 3 else False
-    trans_bit=sys.argv[4] in ['1', 'y', 'yes', 't', 'true'] if argc > 4 else False
+    trans_unit=sys.argv[4].lower() if argc > 4 else '1'
+    trans_bit=sys.argv[5] in ['1', 'y', 'yes', 't', 'true'] if argc > 5 else False
     
-    main(folder, output, do_average, trans_bit)
+    if trans_unit in ['1', ' ', '']: trans_unit=1;
+    elif trans_unit=='k': trans_unit=1000;
+    elif trans_unit=='m': trans_unit=1000*1000;
+    elif trans_unit=='g': trans_unit=1000*1000*1000;
+    else:
+        print('ERROR: unsupported trans-to-kmg option')
+        exit()
+
+    main(folder, output, do_average, trans_unit, trans_bit)
 
 
