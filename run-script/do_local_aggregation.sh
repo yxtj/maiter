4d6eff9b59299aa5b2e98138eab19ed0c7423f19
yxtj <yanxiangtianji@gmail.com>
2018-02-12 03:55:41 +0800

    fix a calling bug for the do_local_aggregate.sh script

--- run-script/do_local_aggregation.sh
+++ run-script/do_local_aggregation.sh
@@ -3,5 +3,5 @@ if [ $# -ne 1 ]; then
 fi
 
 LOCAL_AGG_APPLICATION="Pagerank Adsorption Katz MarkovChain Jacobi"
-
-./contains.sh "$LOCAL_AGG_APPLICATION" $1
+DIR=$(dirname $0)
+$DIR/contains.sh "$LOCAL_AGG_APPLICATION" $1
